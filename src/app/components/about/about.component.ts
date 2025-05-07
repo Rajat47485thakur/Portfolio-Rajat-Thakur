1e8acf512d6578a4c3e7bbc541d2df267a105284
Rajat47485thakur <121743303+Rajat47485thakur@users.noreply.github.com>
2025-05-07 20:30:03 +0530

    added the about animation and the UI for

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,32 @@
-import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  @ViewChild('aboutSection') aboutSection!: ElementRef;
+  isInView: boolean = false;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        this.isInView = entry.isIntersecting; // Toggle true/false every time it enters/leaves
+      });
+    }, {
+      threshold: 0.0 // Adjust based on how much of the section must be visible
+    });
+
+    observer.observe(this.aboutSection.nativeElement);
+  }
 
   getExperienceYears(): number {
     const startDate = new Date(2024, 2); // April 2021
@@ -16,5 +35,6 @@ export class AboutComponent {
     const m = currentDate.getMonth() - startDate.getMonth();
     return m < 0 || (m === 0 && currentDate.getDate() < startDate.getDate()) ? years - 1 : years;
   }
+
 
 }
