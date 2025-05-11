c915d8ad4f39927311e93925d0026ff82383693d
Rajat47485thakur <121743303+Rajat47485thakur@users.noreply.github.com>
2025-05-11 21:47:54 +0530

    Added the ohne and whatsapp

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -15,4 +15,17 @@ export class ContactComponent {
   get email(): string {
     return 'rajatthakurdev24@gmail.com';
   }
+
+  goToWhatsapp(): void {
+    const phoneNumber = '917018683789';
+    const whatsappUrl = `https://wa.me/${phoneNumber}`;
+    window.open(whatsappUrl, '_blank');
+  }
+
+  goToPhone(): void {
+    const phoneNumber = '+917018683789';
+    const telUrl = `tel:${phoneNumber}`;
+    window.open(telUrl, '_self');
+  }
+
 }
